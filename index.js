2e5be3994a0d931ff2c0960fd4e02d3b27eddecd
Moniruzzaman Sujon <git.sujon@gmail.com>
2023-02-13 00:04:11 +0600

    MongoDb Setup done

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors')
-const port = process.env.PORT || 5000
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
+const port = process.env.PORT || 5000;
+
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.prabmlk.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  const usersCollection = client.db("hubx").collection("usersCollection");
+
+  try {
+   
 
 
-app.get('/', (req, res)=> {
-    res.send('Hux Server running')
-})
+  } finally {
+  }
+};
 
-app.listen(port, ()=> {
-    console.log(port, 'Alive Server')
-})
+run().catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hux Server running");
+});
+
+app.listen(port, () => {
+  console.log(port, "Alive Server");
+});
